5b7595b6c9058d16382d767929229a8c16d179d1
Yevgeny Antonov <yevgeny1978@mail.ru>
2022-05-17 18:12:47 +0700

    Refactoring
    improvements to the code.

--- 3DView/mesh.cpp
+++ 3DView/mesh.cpp
@@ -2,43 +2,51 @@
 
 void Mesh::DrawGuro(int nX, int nY, double dScale) const
 {
-	for (const auto& objFace : m_vecFace)
+	for (const auto& objFace : m_vecFaces)
 	{
-		double dLengthNormal = sqrt(objFace.vertex1.dXN * objFace.vertex1.dXN + objFace.vertex1.dYN * objFace.vertex1.dYN + objFace.vertex1.dZN * objFace.vertex1.dZN);
-		double ax = objFace.vertex1.dXN / dLengthNormal;
-		double ay = objFace.vertex1.dYN / dLengthNormal;
-		double az = objFace.vertex1.dZN / dLengthNormal;
-		double dIntensity1 = abs(m_dLightX * ax + m_dLightY * ay + m_dLightZ * az) * 255;
-		dLengthNormal = sqrt(objFace.vertex2.dXN * objFace.vertex2.dXN + objFace.vertex2.dYN * objFace.vertex2.dYN + objFace.vertex2.dZN * objFace.vertex2.dZN);
-		ax = objFace.vertex2.dXN / dLengthNormal;
-		ay = objFace.vertex2.dYN / dLengthNormal;
-		az = objFace.vertex2.dZN / dLengthNormal;
-		double dIntensity2 = abs(m_dLightX * ax + m_dLightY * ay + m_dLightZ * az) * 255;
-		dLengthNormal = sqrt(objFace.vertex3.dXN * objFace.vertex3.dXN + objFace.vertex3.dYN * objFace.vertex3.dYN + objFace.vertex3.dZN * objFace.vertex3.dZN);
-		ax = objFace.vertex3.dXN / dLengthNormal;
-		ay = objFace.vertex3.dYN / dLengthNormal;
-		az = objFace.vertex3.dZN / dLengthNormal;
-		double dIntensity3 = abs(m_dLightX * ax + m_dLightY * ay + m_dLightZ * az) * 255;
-		double x1 = m_dDist * dScale * objFace.vertex1.dX / (m_dDist - objFace.vertex1.dZ * dScale) + nX;
-		double x2 = m_dDist * dScale * objFace.vertex2.dX / (m_dDist - objFace.vertex2.dZ * dScale) + nX;
-		double x3 = m_dDist * dScale * objFace.vertex3.dX / (m_dDist - objFace.vertex3.dZ * dScale) + nX;
-		double y1 = m_dDist * dScale * objFace.vertex1.dY / (m_dDist - objFace.vertex1.dZ * dScale) + nY;
-		double y2 = m_dDist * dScale * objFace.vertex2.dY / (m_dDist - objFace.vertex2.dZ * dScale) + nY;
-		double y3 = m_dDist * dScale * objFace.vertex3.dY / (m_dDist - objFace.vertex3.dZ * dScale) + nY;
-		TriangleGradient(x1, y1, objFace.vertex1.dZ * dScale, x2, y2, objFace.vertex2.dZ * dScale, x3, y3, objFace.vertex3.dZ * dScale, dIntensity1, dIntensity2, dIntensity3);
+		double dLengthNormal = std::sqrt(m_vecNormales[objFace.iEl1].dX * m_vecNormales[objFace.iEl1].dX +
+			m_vecNormales[objFace.iEl1].dY * m_vecNormales[objFace.iEl1].dY + 
+			m_vecNormales[objFace.iEl1].dZ * m_vecNormales[objFace.iEl1].dZ);
+		double ax = m_vecNormales[objFace.iEl1].dX / dLengthNormal;
+		double ay = m_vecNormales[objFace.iEl1].dY / dLengthNormal;
+		double az = m_vecNormales[objFace.iEl1].dZ / dLengthNormal;
+		double dIntensity1 = abs(m_TLight.dX * ax + m_TLight.dY * ay + m_TLight.dZ * az) * 255;
+		dLengthNormal = std::sqrt(m_vecNormales[objFace.iEl2].dX * m_vecNormales[objFace.iEl2].dX +
+			m_vecNormales[objFace.iEl2].dY * m_vecNormales[objFace.iEl2].dY +
+			m_vecNormales[objFace.iEl2].dZ * m_vecNormales[objFace.iEl2].dZ);
+		ax = m_vecNormales[objFace.iEl2].dX / dLengthNormal;
+		ay = m_vecNormales[objFace.iEl2].dY / dLengthNormal;
+		az = m_vecNormales[objFace.iEl2].dZ / dLengthNormal;
+		double dIntensity2 = abs(m_TLight.dX * ax + m_TLight.dY * ay + m_TLight.dZ * az) * 255;
+		dLengthNormal = std::sqrt(m_vecNormales[objFace.iEl3].dX * m_vecNormales[objFace.iEl3].dX +
+			m_vecNormales[objFace.iEl3].dY * m_vecNormales[objFace.iEl3].dY +
+			m_vecNormales[objFace.iEl3].dZ * m_vecNormales[objFace.iEl3].dZ);
+		ax = m_vecNormales[objFace.iEl3].dX / dLengthNormal;
+		ay = m_vecNormales[objFace.iEl3].dY / dLengthNormal;
+		az = m_vecNormales[objFace.iEl3].dZ / dLengthNormal;
+		double dIntensity3 = abs(m_TLight.dX * ax + m_TLight.dY * ay + m_TLight.dZ * az) * 255;
+		double x1 = m_dDist * dScale * m_vecVertices[objFace.iEl1].dX / (m_dDist - m_vecVertices[objFace.iEl1].dZ * dScale) + nX;
+		double x2 = m_dDist * dScale * m_vecVertices[objFace.iEl2].dX / (m_dDist - m_vecVertices[objFace.iEl1].dZ * dScale) + nX;
+		double x3 = m_dDist * dScale * m_vecVertices[objFace.iEl3].dX / (m_dDist - m_vecVertices[objFace.iEl1].dZ * dScale) + nX;
+		double y1 = m_dDist * dScale * m_vecVertices[objFace.iEl1].dY / (m_dDist - m_vecVertices[objFace.iEl1].dZ * dScale) + nY;
+		double y2 = m_dDist * dScale * m_vecVertices[objFace.iEl2].dY / (m_dDist - m_vecVertices[objFace.iEl2].dZ * dScale) + nY;
+		double y3 = m_dDist * dScale * m_vecVertices[objFace.iEl3].dY / (m_dDist - m_vecVertices[objFace.iEl3].dZ * dScale) + nY;
+		TriangleGradient(x1, y1, m_vecVertices[objFace.iEl1].dZ * dScale, x2, y2, m_vecVertices[objFace.iEl2].dZ * dScale, x3, y3, 
+			m_vecVertices[objFace.iEl3].dZ * dScale, dIntensity1, dIntensity2, dIntensity3);
 	}
 }
 
 void Mesh::Draw(int nX, int nY, double dScale) const
 {
-	for (const auto& objFace : m_vecFace)
+	for (const auto& objFace : m_vecFaces)
 	{
-		double ax = objFace.vertex1.dX - objFace.vertex2.dX;
-		double ay = objFace.vertex1.dY - objFace.vertex2.dY;
-		double az = objFace.vertex1.dZ - objFace.vertex2.dZ;
-		double bx = objFace.vertex2.dX - objFace.vertex3.dX;
-		double by = objFace.vertex2.dY - objFace.vertex3.dY;
-		double bz = objFace.vertex2.dZ - objFace.vertex3.dZ;
+		double ax = m_vecVertices[objFace.iEl1].dX - m_vecVertices[objFace.iEl2].dX;
+		double ay = m_vecVertices[objFace.iEl1].dY - m_vecVertices[objFace.iEl2].dY;
+		double az = m_vecVertices[objFace.iEl1].dZ - m_vecVertices[objFace.iEl2].dZ;
+
+		double bx = m_vecVertices[objFace.iEl2].dX - m_vecVertices[objFace.iEl3].dX;
+		double by = m_vecVertices[objFace.iEl2].dY - m_vecVertices[objFace.iEl3].dY;
+		double bz = m_vecVertices[objFace.iEl2].dZ - m_vecVertices[objFace.iEl3].dZ;
 		double dNormalX = ay * bz - az * by;
 		double dNormalY = az * bx - ax * bz;
 		double dNormalZ = ax * by - ay * bx;
@@ -46,52 +54,54 @@ void Mesh::Draw(int nX, int nY, double dScale) const
 		dNormalX = dNormalX / dLengthNormal;
 		dNormalY = dNormalY / dLengthNormal;
 		dNormalZ = dNormalZ / dLengthNormal;
-		double dIntensity = abs(m_dLightX * dNormalX + m_dLightY * dNormalY + m_dLightZ * dNormalZ);
-		double x1 = m_dDist * dScale * objFace.vertex1.dX / (m_dDist - objFace.vertex1.dZ * dScale) + nX;
-		double x2 = m_dDist * dScale * objFace.vertex2.dX / (m_dDist - objFace.vertex2.dZ * dScale) + nX;
-		double x3 = m_dDist * dScale * objFace.vertex3.dX / (m_dDist - objFace.vertex3.dZ * dScale) + nX;
-		double y1 = m_dDist * dScale * objFace.vertex1.dY / (m_dDist - objFace.vertex1.dZ * dScale) + nY;
-		double y2 = m_dDist * dScale * objFace.vertex2.dY / (m_dDist - objFace.vertex2.dZ * dScale) + nY;
-		double y3 = m_dDist * dScale * objFace.vertex3.dY / (m_dDist - objFace.vertex3.dZ * dScale) + nY;
-		Triangle(x1, y1, objFace.vertex1.dZ * dScale, x2, y2, objFace.vertex2.dZ * dScale, x3, y3, objFace.vertex3.dZ * dScale, dIntensity * 255);
+		double dIntensity = abs(m_TLight.dX * dNormalX + m_TLight.dY * dNormalY + m_TLight.dZ * dNormalZ) * 255;
+		double x1 = m_dDist * dScale * m_vecVertices[objFace.iEl1].dX / (m_dDist - m_vecVertices[objFace.iEl1].dZ * dScale) + nX;
+		double x2 = m_dDist * dScale * m_vecVertices[objFace.iEl2].dX / (m_dDist - m_vecVertices[objFace.iEl1].dZ * dScale) + nX;
+		double x3 = m_dDist * dScale * m_vecVertices[objFace.iEl3].dX / (m_dDist - m_vecVertices[objFace.iEl1].dZ + dScale) + nX;
+		double y1 = m_dDist * dScale * m_vecVertices[objFace.iEl1].dY / (m_dDist - m_vecVertices[objFace.iEl1].dZ * dScale) + nY;
+		double y2 = m_dDist * dScale * m_vecVertices[objFace.iEl2].dY / (m_dDist - m_vecVertices[objFace.iEl2].dZ * dScale) + nY;
+		double y3 = m_dDist * dScale * m_vecVertices[objFace.iEl3].dY / (m_dDist - m_vecVertices[objFace.iEl3].dZ * dScale) + nY;
+		Triangle(x1, y1, m_vecVertices[objFace.iEl1].dZ * dScale, x2, y2, m_vecVertices[objFace.iEl2].dZ * dScale, x3, y3, 
+			 m_vecVertices[objFace.iEl3].dZ * dScale, dIntensity);
 	}
 }
 
 void Mesh::Rotate(double dAngleX)
 {
-	const double dCosConst = cos(dAngleX * DEG);
-	const double dSinConst = sin(dAngleX * DEG);
-	for (auto& objFace : m_vecFace)
+	const double dCosConst = std::cos(dAngleX * DEG);
+	const double dSinConst = std::sin(dAngleX * DEG);
+	// Rotate Vertices
+	for (auto& objVertex : m_vecVertices)
 	{
-		// Rotate vertexes
-		double dTemp = objFace.vertex1.dX * dCosConst - objFace.vertex1.dZ * dSinConst;
-		objFace.vertex1.dZ = objFace.vertex1.dX * dSinConst + objFace.vertex1.dZ * dCosConst;
-		objFace.vertex1.dX = dTemp;
-		dTemp = objFace.vertex2.dX * dCosConst - objFace.vertex2.dZ * dSinConst;
-		objFace.vertex2.dZ = objFace.vertex2.dX * dSinConst + objFace.vertex2.dZ * dCosConst;
-		objFace.vertex2.dX = dTemp;
-		dTemp = objFace.vertex3.dX * dCosConst - objFace.vertex3.dZ * dSinConst;
-		objFace.vertex3.dZ = objFace.vertex3.dX * dSinConst + objFace.vertex3.dZ * dCosConst;
-		objFace.vertex3.dX = dTemp;
+		
+		double dTemp = objVertex.dX * dCosConst - objVertex.dZ * dSinConst;
+		objVertex.dZ = objVertex.dX * dSinConst + objVertex.dZ * dCosConst;
+		objVertex.dX = dTemp;
+		dTemp = objVertex.dX * dCosConst - objVertex.dZ * dSinConst;
+		objVertex.dZ = objVertex.dX * dSinConst + objVertex.dZ * dCosConst;
+		objVertex.dX = dTemp;
+		dTemp = objVertex.dX * dCosConst - objVertex.dZ * dSinConst;
+		objVertex.dZ = objVertex.dX * dSinConst + objVertex.dZ * dCosConst;
+		objVertex.dX = dTemp;
+	}
 
-		// Rotate normals
-		dTemp = objFace.vertex1.dXN * dCosConst - objFace.vertex1.dZN * dSinConst;
-		objFace.vertex1.dZN = objFace.vertex1.dXN * dSinConst + objFace.vertex1.dZN * dCosConst;
-		objFace.vertex1.dXN = dTemp;
-		dTemp = objFace.vertex2.dXN * dCosConst - objFace.vertex2.dZN * dSinConst;
-		objFace.vertex2.dZN = objFace.vertex2.dXN * dSinConst + objFace.vertex2.dZN * dCosConst;
-		objFace.vertex2.dXN = dTemp;
-		dTemp = objFace.vertex3.dXN * dCosConst - objFace.vertex3.dZN * dSinConst;
-		objFace.vertex3.dZN = objFace.vertex3.dXN * dSinConst + objFace.vertex3.dZN * dCosConst;
-		objFace.vertex3.dXN = dTemp;
+	// Rotate Normales
+	for (auto& objVertex : m_vecNormales)
+	{
+		double dTemp = objVertex.dX * dCosConst - objVertex.dZ * dSinConst;
+		objVertex.dZ = objVertex.dX * dSinConst + objVertex.dZ * dCosConst;
+		objVertex.dX = dTemp;
+		dTemp = objVertex.dX * dCosConst - objVertex.dZ * dSinConst;
+		objVertex.dZ = objVertex.dX * dSinConst + objVertex.dZ * dCosConst;
+		objVertex.dX = dTemp;
+		dTemp = objVertex.dX * dCosConst - objVertex.dZ * dSinConst;
+		objVertex.dZ = objVertex.dX * dSinConst + objVertex.dZ * dCosConst;
+		objVertex.dX = dTemp;
 	}
 }
 
 void Mesh::CreateFromFile(const std::string& sFileName)
 {
-	struct Point { double x, y, z; };
-	std::vector<Point> vecVertex;
-	std::vector<Point> vecNormal;
 	std::ifstream in(sFileName);
 	if (in)
 	{
@@ -99,7 +109,7 @@ void Mesh::CreateFromFile(const std::string& sFileName)
 		in.seekg(0, in.end);
 		size_t lengthOfFile = in.tellg();
 		in.seekg(0, in.beg);
-		auto DrawProgressWidget = [&in, &lengthOfFile]()
+		auto DrawProgressBar = [&in, &lengthOfFile]()
 		{
 			while (true)
 			{
@@ -119,9 +129,32 @@ void Mesh::CreateFromFile(const std::string& sFileName)
 			SDL_RenderFillRect(renderer, &Rect);
 			SDL_RenderPresent(renderer);
 		};
+
+		auto Split = [](const std::string_view& s, char sep = ' ') -> std::vector<std::string>
+		{
+			std::vector<std::string> vecStrings;
+			auto it1 = s.begin();
+			bool bIsNotSeparator = false;
+			for (auto it = s.begin(); it != s.end(); ++it)
+			{
+				if (*it != sep && !bIsNotSeparator)
+				{
+					it1 = it;
+					bIsNotSeparator = true;
+				}
+				if (*it == sep && bIsNotSeparator)
+				{
+					bIsNotSeparator = false;
+					vecStrings.emplace_back(it1, it);
+				}
+			}
+			if (bIsNotSeparator)
+				vecStrings.emplace_back(it1, s.end());
+			return vecStrings;
+		};
 		
 
-		std::thread threadOfProgressBar(DrawProgressWidget);
+		std::thread threadOfProgressBar(DrawProgressBar);
 
 		std::string line;
 		while (std::getline(in, line))
@@ -131,31 +164,23 @@ void Mesh::CreateFromFile(const std::string& sFileName)
 			{
 				if (vecStr[0] == "v")
 				{
-					vecVertex.emplace_back(Point{ std::stod(vecStr[1]), -std::stod(vecStr[2]), std::stod(vecStr[3]) });
+					m_vecVertices.emplace_back(Vertex{ std::stod(vecStr[1]), -std::stod(vecStr[2]), std::stod(vecStr[3]) });
 				}
 				if (vecStr[0] == "vn")
 				{
-					vecNormal.emplace_back(Point{ std::stod(vecStr[1]), std::stod(vecStr[2]), std::stod(vecStr[3]) });
+					m_vecNormales.emplace_back(Vertex{ std::stod(vecStr[1]), std::stod(vecStr[2]), std::stod(vecStr[3]) });
 				}
 				if (vecStr[0] == "f")
 				{
 					std::vector<std::string> vecStr1 = Split(vecStr[1], '/');
 					std::vector<std::string> vecStr2 = Split(vecStr[2], '/');
 					std::vector<std::string> vecStr3 = Split(vecStr[3], '/');
-					m_vecFace.emplace_back(Face{
-						Vertex{vecVertex[std::stoi(vecStr1[0]) - 1].x, vecVertex[std::stoi(vecStr1[0]) - 1].y, vecVertex[std::stoi(vecStr1[0]) - 1].z, 0.0, 0.0, 0.0},
-						Vertex{vecVertex[std::stoi(vecStr2[0]) - 1].x, vecVertex[std::stoi(vecStr2[0]) - 1].y, vecVertex[std::stoi(vecStr2[0]) - 1].z, 0.0, 0.0, 0.0},
-						Vertex{vecVertex[std::stoi(vecStr3[0]) - 1].x, vecVertex[std::stoi(vecStr3[0]) - 1].y, vecVertex[std::stoi(vecStr3[0]) - 1].z, 0.0, 0.0, 0.0},
-						});
+					m_vecFaces.emplace_back(Face{std::stoi(vecStr1[0]) - 1, std::stoi(vecStr2[0]) - 1, std::stoi(vecStr3[0]) - 1});
 
 					if (vecStr.size() > 4)
 					{
 						std::vector<std::string> vecStr4 = Split(vecStr[4], '/');
-						m_vecFace.emplace_back(Face{
-							Vertex{vecVertex[std::stoi(vecStr1[0]) - 1].x, vecVertex[std::stoi(vecStr1[0]) - 1].y, vecVertex[std::stoi(vecStr1[0]) - 1].z, 0.0, 0.0, 0.0},
-							Vertex{vecVertex[std::stoi(vecStr3[0]) - 1].x, vecVertex[std::stoi(vecStr3[0]) - 1].y, vecVertex[std::stoi(vecStr3[0]) - 1].z, 0.0, 0.0, 0.0},
-							Vertex{vecVertex[std::stoi(vecStr4[0]) - 1].x, vecVertex[std::stoi(vecStr4[0]) - 1].y, vecVertex[std::stoi(vecStr4[0]) - 1].z, 0.0, 0.0, 0.0},
-							});
+						m_vecFaces.emplace_back(Face{ std::stoi(vecStr1[0]) - 1, std::stoi(vecStr3[0]) - 1, std::stoi(vecStr4[0]) - 1 });
 					}
 				}
 			}
@@ -165,25 +190,38 @@ void Mesh::CreateFromFile(const std::string& sFileName)
 	}
 }
 
-std::vector<std::string> Mesh::Split(const std::string_view& s, char sep) const
+
+void Mesh::CorrectXYZ()
 {
-	std::vector<std::string> vecStrings;
-	auto it1 = s.begin();
-	bool bIsNotSeparator = false;
-	for (auto it = s.begin(); it != s.end(); ++it)
+	double dXMin = 0.0;
+	double dXMax = 0.0;
+	double dYMin = 0.0;
+	double dYMax = 0.0;
+	double dZMin = 0.0;
+	double dZMax = 0.0;
+	
+	for (const auto& objVertex : m_vecVertices)
 	{
-		if (*it != sep && !bIsNotSeparator)
-		{
-			it1 = it;
-			bIsNotSeparator = true;
-		}
-		if (*it == sep && bIsNotSeparator)
-		{
-			bIsNotSeparator = false;
-			vecStrings.emplace_back(it1, it);
-		}
+		if(objVertex.dX < dXMin) dXMin = objVertex.dX;
+		if(dXMax < objVertex.dX) dXMax = objVertex.dX;
+		if(objVertex.dY < dYMin) dYMin = objVertex.dY;
+		if(dYMax < objVertex.dY) dYMax = objVertex.dY;
+		if(objVertex.dZ < dZMin) dZMin = objVertex.dZ;
+		if(dZMax < objVertex.dZ) dZMax = objVertex.dZ;
 	}
-	if (bIsNotSeparator)
-		vecStrings.emplace_back(it1, s.end());
-	return vecStrings;
+
+	double dShiftX = abs(dXMax + dXMin) / 2;
+	double dShiftY = abs(dYMax + dYMin) / 2;
+	double dShiftZ = abs(dZMax + dZMin) / 2;
+
+	if(abs(dXMax) > abs(dXMin)) dShiftX = -dShiftX;
+	if(abs(dYMax) > abs(dYMin)) dShiftY = -dShiftY;
+	if(abs(dZMax) > abs(dZMin)) dShiftZ = -dShiftZ;
+
+	for (auto& objVertex : m_vecVertices)
+	{
+		objVertex.dX += dShiftX;
+		objVertex.dY += dShiftY;
+		objVertex.dZ += dShiftZ;
+	}
 }
